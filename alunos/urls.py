112aef6e010d1049e181e5c5f6925983547b77c4
bernardesraischruama <bernardesraichruama@gmail.com>
2025-02-17 10:56:10 -0300

    Introdução API REST

---
+++ alunos/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from .views import EstadoViewSet
+
+router = DefaultRouter()
+router.register(r'estados', EstadoViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('admin/', admin.site.urls),
+]
